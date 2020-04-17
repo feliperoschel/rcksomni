6f1a1af3327444c33c4c087617b7e340b324a9a1
Felipe Roschel <f.roschel@gmail.com>
2020-04-17 00:13:46 -0300

    test

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
-import api from '../../services/api'; 
+import api from '../../services/api';
 
 import './styles.css';
 

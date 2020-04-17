10d82f2a677960c0ee00dc6b53c8ff8d10c04811
Felipe Roschel <f.roschel@gmail.com>
2020-04-17 17:33:08 -0300

    style guide

--- backend/src/database/migrations/20200413175343_create_ongs.js
+++ backend/src/database/migrations/20200413175343_create_ongs.js
@@ -1,15 +1,11 @@
 
-exports.up = function(knex) {
-  return knex.schema.createTable('ongs', function(table) {
-    table.string('id').primary();
-    table.string('name').notNullable();
-    table.string('email').notNullable();
-    table.string('whatsapp').notNullable();
-    table.string('city').notNullable();
-    table.string('uf').notNullable();
-  });
-};
+exports.up = (knex) => knex.schema.createTable('ongs', (table) => {
+  table.string('id').primary();
+  table.string('name').notNullable();
+  table.string('email').notNullable();
+  table.string('whatsapp').notNullable();
+  table.string('city').notNullable();
+  table.string('uf').notNullable();
+});
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('ongs');
-};
+exports.down = (knex) => knex.schema.dropTable('ongs');
--- backend/src/database/migrations/20200413180041_create_incidents.js
+++ backend/src/database/migrations/20200413180041_create_incidents.js
@@ -1,15 +1,11 @@
 
-exports.up = function(knex) {
-  return knex.schema.createTable('incidents', function(table) {
-    table.increments();
-    table.string('title').notNullable();
-    table.string('description').notNullable();
-    table.decimal('value').notNullable();
-    table.string('ong_id').notNullable();
-    table.foreign('ong_id').references('id').inTable('ongs');
-  });
-};
+exports.up = (knex) => knex.schema.createTable('incidents', (table) => {
+  table.increments();
+  table.string('title').notNullable();
+  table.string('description').notNullable();
+  table.decimal('value').notNullable();
+  table.string('ong_id').notNullable();
+  table.foreign('ong_id').references('id').inTable('ongs');
+});
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('incidents');
-};
+exports.down = (knex) => knex.schema.dropTable('incidents');
